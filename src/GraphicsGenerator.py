c66561760390673f2a87babdbc24ebf4e65326ed
unknown <user@20171068-p01.unist.ac.kr>
2022-11-15 22:01:53 +0900

    wall and tiles

--- src/GraphicsGenerator.py
+++ src/GraphicsGenerator.py
@@ -1,31 +1,77 @@
 # This is the graphics Generator that generates the graphics based on the info
 from CharacterController import *
 
+from tkinter import Y
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import numpy as np
+
 class GraphicsGenerator:
     def __init__(self, maze):
         self.maze = maze
         self.characterController = CharacterController()
+        self.tile_size = 2.0
+        self.height = -1.25
 
-    def GenerateGraphics(self):
-        pass
+        self.trans_mat = np.eye(4)
+        self.scale_mat = np.eye(4)
+        self.rot_mat = np.eye(4)
 
     def GenerateTiles(self):
-        """
-        Single Local Tile is 4 * 4 cube
-        """
-        pass
+        # y value is fixed. and then x/z vaule varies.
+        for i in range(4):
+            for j in range(4):
+                if i%2==j%2:
+                    color = 'black'
+                else:
+                    color = 'white'
+                    # checker shape
+                self.GenerateSingleBlock((-1.5+i)*self.tile_size,self.height,(-1.5+j)*self.tile_size,self.tile_size,color)
 
-    def GenerateWalls(self):
-        for ~~ :
-            self.generate_single_block(~~)
-        pass
-    def generate_single_block(self, x,y,z):
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
+    def GenerateWalls(self, test: bool=False):
+        size = self.tile_size/4
+        color = 'white'
+        threshold = 0
+        edge_location=1.875*self.tile_size
+        
+        for i in range(16):
+            for j in range(9):
+                if i%2 == j%2:
+                    color = 'black'
+                else:
+                    color = 'white'
+                    # checker shape
+                if test == True:
+                    self.GenerateSingleBlock(edge_location, j*size + threshold, -edge_location + size * i, size, color)
+                else:
+                    self.GenerateSingleBlock(-edge_location + size * i, j*size + threshold, -edge_location, size, color)
+                    # ** Notice: -edge_location+size*i or +edge_location+size*i will give opposite side of edges
+                    # ** Notice: Changing x and z coordinate will give wall at other axis
+
+    def GenerateSingleBlock(self, x,y,z, size=0.25, color='black'):
+        # **!! This function must be just under the cam setting and transformation matrix !!**
+        # **!! Not followed by glLoadIdentity() !!**
+        # **!! glLoadIdentity() must be followed after every single block has been generated !!**
         trans_mat = np.eye(4)
-        glMultMatrixf((trans_mat).T) #define matrices
-        glutSolidCube(0.25)
-        glLoadIdentity()
+        trans_mat[0,3] = x
+        trans_mat[1,3] = y
+        trans_mat[2,3] = z
+        trans_mat_i = np.eye(4)
+        trans_mat_i[0,3] = -x
+        trans_mat_i[1,3] = -y
+        trans_mat_i[2,3] = -z
+        if color == 'black':
+            glColor3f(0.2,0.2,0.2)
+        elif color == 'white':
+            glColor3f(1.0,1.0,1.0)
+        else:
+            glColor3f(0.75,0.125,0.25)
+
+        glMultMatrixf(trans_mat.T) #define matrices
+        glutSolidCube(size)
+        glMultMatrixf(trans_mat_i.T)
+        glColor3f(1.0,1.0,1.0)
 
     def GenerateMarks(self):
         pass
@@ -41,14 +87,37 @@ class GraphicsGenerator:
 
     def GenerateClock(self):
         pass
+    
+    def perspective(self, fov):
+        self.camz = self.h/800/np.tan((np.pi/180)*fov/2)
+        self.nearz = self.camz / 8 # self.camz - 7/8
+        self.farz = self.camz * 4.5
 
-    def display(self):
+    def GenerateGraphics(self):
         """
         camera view
         tile
         wall
         """
+        # Camera view
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glClearColor(0, 0, 0, 1)
 
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        self.perspective(45)
+        glFrustum(-self.w/800/8,self.w/800/8,-self.h/800/8,self.h/800/8, self.nearz, self.farz)
+
+        glMatrixMode(GL_MODELVIEW)
+        glClear(GL_COLOR_BUFFER_BIT)
+        glLoadIdentity()
+        gluLookAt(0,0,self.camz, 0,0,0, 0,1,0)
+        glMultMatrixf((self.trans_mat@self.scale_mat@self.rot_mat).T)
+        # tile
+        self.GenerateTiles()
+        # wall
+        self.GenerateWalls(True)
+        self.GenerateWalls(False)
         pass
     def keyboard(self, key, x, y):
         self.characterController.InputKeyboard(key)
@@ -63,7 +132,7 @@ class GraphicsGenerator:
         glutInitWindowPosition(0, 0)
         glutCreateWindow(b"Maze Simulator")
 
-        glutDisplayFunc(self.display)
+        glutDisplayFunc(self.GenerateGraphics)
         glutKeyboardFunc(self.keyboard)#wasd movement
         glutSpecialFunc(self.special)#Left Right Arrow
         # glutMouseFunc(self.mouse)
